0640af4dc7b96704d3df4be9dc0f59f2f74b35f8
hilary-chol <158524846+Hilary-Chol@users.noreply.github.com>
2025-06-12 09:41:04 +0300

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,48 @@
+print(" enter number")
+
+grade = None
+
+#the progam below should take marks and give grade according to the mark given
+#0-39 F, 40-44 is E-, 45-49 is E, 50-54 is D, 55-59 is D+, 60-64 is C,
+#65-69 C+, 70-74 is B, 75-79 is B+, 80-90 is A and 90-100 is A+
+def grader():
+    mark = int(input())
+    if mark >= 0 and mark <= 100:
+        if(mark >= 90 and mark <= 100):
+            grade = "A+"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 80 and mark <= 90):
+            grade = "A"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 75 and mark < 80):
+            grade = "B+"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 70 and mark < 75):
+            grade = "B"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 65 and mark < 70):
+            grade = "C+"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 60 and mark < 65 ):
+            grade = "C"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 55 and mark < 60):
+            grade = "D+"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 50 and mark < 55):
+            grade = "D"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 45 and mark < 50):
+            grade = "E"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 40 and mark < 45):
+            grade = "E-"
+            print(f"The grade for the mark {mark} is {grade}")
+        elif(mark >= 0 and mark < 40):
+            grade = "F"
+            print(f"The grade for the mark {mark} is {grade}")
+    else: 
+        print("please enter a number between 0 and 100")
+        grader()
+
+grader()
---
+++ stringtointeger.py
@@ -0,0 +1,6 @@
+age = "8"
+numAge = int(age)
+
+#to check if the datatype of the variable age is changed from string to an integer
+#the program below gives you the value of age as an integer multuplied by two
+print(f"::{numAge * 2}::")
